d7ff3d3ed959ebe07da1b8baf86a7a96cb64f12f
raymondzhu2020 <58727770+raymondzhu2020@users.noreply.github.com>
2019-12-19 17:47:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Greeting, New Guys!');
+  res.send('Hello World, Epam Guys');
 });
 
 app.listen(3000, function () {
